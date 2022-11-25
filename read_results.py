c2704251790c1c74e88855ba803d004aa4b7c349
CharlotteHoffmans <79145595+CharlotteHoffmans@users.noreply.github.com>
2022-11-25 19:52:25 +0100

    results

--- read_results.py
+++ read_results.py
@@ -2,7 +2,7 @@ import pandas as pd
 import calendar
 import datetime, time
 import numpy as np
-df = pd.read_csv('results_basic_dpll_9x9.csv')
+df = pd.read_csv('TEST2_results_JW1.csv')
 results = df[["Runtime", "Computationaltime", "Backtracks"]]
 # print(results)
 
@@ -10,6 +10,7 @@ results["Runtime"]= pd.to_timedelta(results["Runtime"]).view(np.int64) / 1e9
 results["Computationaltime"]= pd.to_timedelta(results["Computationaltime"]).view(np.int64) / 1e9
 
 results.loc['mean'] = results.mean()
+results.loc['std'] = results.std()
 
 print(results)
-results.to_csv("Results/final_results_DPLL_9x9.csv", index=False)
+results.to_csv("Results/final_results_JW1_9x9.csv", index=False)
